fc552be4a1e0130939668845e48987967ce9deeb
luizlimadev <devluizlima@gmail.com>
2023-02-27 20:40:36 -0300

    efeito scroll para animacoes

---
+++ src/js/scroll-animacao.js
@@ -0,0 +1,19 @@
+
+const checaID = document.querySelectorAll(".animacaoPausada");
+
+function animacaoScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    checaID.forEach((elemento) => {
+        if ((windowTop) > elemento.offsetTop) {
+            elemento.style = "animation-play-state: running";
+        }
+    });
+}
+
+animacaoScroll();
+
+if (checaID.length) {
+    window.addEventListener("scroll", function () {
+        animacaoScroll();
+    });
+}
